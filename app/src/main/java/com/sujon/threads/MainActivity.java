6df5c890cc04b7e62e24168250cb29f0f1869f79
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-06 02:03:47 +0600

    1_What is Thread - Android Development with Java - Rakibul Islam - Bohubrihi
    1_What is Thread - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/threads/MainActivity.java
+++ app/src/main/java/com/sujon/threads/MainActivity.java
@@ -1,13 +1,37 @@
 package com.sujon.threads;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView myTextView;
+    EditText myEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myEditText=findViewById(R.id.editTextTextPersonName);
+        myTextView=findViewById(R.id.textView);
+
     }
+
+    public void OnButtonClick(View view){
+        synchronized (this){
+            try {
+                wait(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        myTextView.setText("Text Changed!!");
+
+    }
+
 }
